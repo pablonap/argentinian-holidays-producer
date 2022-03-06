47c37bc7d67e1470b3424f15cb666d5b0c92bacb
“chipo” <pnapoli33@gmail.com>
2022-03-06 14:32:03 -0300

    Add modelmapper dependency; add ApplicationConfig to configure bean for ModelMapper; add HolidayDTO class; add java doc into Holiday class.

---
+++ src/main/java/com/practiceproject/dto/HolidayDTO.java
@@ -0,0 +1,21 @@
+package com.practiceproject.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The {@code HolidayDTO} class represents the data of an
+ * argentinian holiday to be sent whenever the end point is called.
+ *
+ * @author pin1
+ *
+ */
+@Data
+@NoArgsConstructor
+public class HolidayDTO {
+	private Long id;
+
+    private String name;
+
+	private String holidayDate;
+}
--- src/main/java/com/practiceproject/model/Holiday.java
+++ src/main/java/com/practiceproject/model/Holiday.java
@@ -6,9 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.Date;
-
+/**
+ * The {@code Holiday} class represents the data model
+ * for an argentinian holiday.
+ *
+ * @author pablo.napoli
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
