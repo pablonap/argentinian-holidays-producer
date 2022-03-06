058b6006482c327ecd8664b6cf7ec4fbc06e27cc
“chipo” <pnapoli33@gmail.com>
2022-03-06 11:43:17 -0300

    Initial commit

---
+++ src/main/java/com/practiceproject/ArgentinianHolidaysProducerApplication.java
@@ -0,0 +1,13 @@
+package com.practiceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgentinianHolidaysProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgentinianHolidaysProducerApplication.class, args);
+	}
+
+}
